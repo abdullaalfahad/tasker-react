d55a0acc5eacd749b2bf5f88bb1fa7b3762a9129
abdullaalfahad <aafahad02@gmail.com>
2025-07-01 18:58:16 +0600

    feat: hero section added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import HeroScetion from "./components/HeroScetion";
 
 function App() {
   return (
     <>
       <Header />
+      <div className="flex flex-col justify-center items-center">
+        <HeroScetion />
+      </div>
       <Footer />
     </>
   );
