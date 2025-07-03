0eb0c62f1b00abf0cb121172dcbb1d6b7e5b8a5c
abdullaalfahad <aafahad02@gmail.com>
2025-07-03 20:20:03 +0600

    refactor: task action and task lists separated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ export default function Header() {
   return (
     <nav class="py-6 md:py-8 w-full">
       <div class="container mx-auto flex items-center justify-center gap-x-6">
-        <a href="/" className="text-4xl">
+        <a href="/" className="text-4xl text-[#F5BF42]">
           Tasker React
         </a>
       </div>
