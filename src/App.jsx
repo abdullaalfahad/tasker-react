8087ea212289e9fa466794e4e32c03dd2c76bf1d
abdullaalfahad <aafahad02@gmail.com>
2025-07-03 20:16:21 +0600

    chore: search task separated

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import HeroScetion from "./components/HeroScetion";
+import TaskBoard from "./components/task/TaskBoard";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Header />
       <div className="flex flex-col justify-center items-center">
         <HeroScetion />
+        <TaskBoard />
       </div>
       <Footer />
     </>
